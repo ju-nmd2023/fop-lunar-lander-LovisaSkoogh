72713579801d1b42f8e3cd25f269169172b177c9
LovisaSkoogh <sklo23zz@student.ju.se>
2024-02-22 14:31:02 +0100

    final changes

--- .github/lunarlander.js
+++ .github/lunarlander.js
@@ -3,6 +3,7 @@ let velocityY = 0;
 let speed = 0;
 let gravity = 0.1;
 let gameOverText = false;
+let youWinText = false;
 let startScreen = true;
 let engineFire = false;
 
@@ -10,6 +11,7 @@ function rocketship(x, y) {
   push();
   translate(300, y);
   scale(0.25);
+  // the following "rocketship" code was adapted from adapted from https://editor.p5js.org/rayonmp/sketches/qk8--NFl4 Accessed: 2024-02-12
   // wings
   stroke(33, 5, 19);
   fill(132, 22, 77);
@@ -92,7 +94,7 @@ function draw() {
     pop();
     // moon
     noStroke();
-    fill(212, 212, 212);
+    fill(170);
     ellipse(300, 760, 800, 600, PI, 0);
 
     rocketship(300, y);
@@ -103,7 +105,8 @@ function draw() {
 
     y += velocityY * speed;
 
-    if (keyIsDown(38)) {
+    // ENGINE FIRE
+    if (keyIsDown(32)) {
       speed = -0.6;
       engineFire = true;
     } else {
@@ -116,12 +119,28 @@ function draw() {
       y += velocityY * speed;
     } else {
       y = 400;
-
       speed = 0;
     }
 
+    // WIN SCREEN
+    if (youWinText) {
+      stroke(255);
+      fill(255, 204, 204);
+      rect(50, 200, 600, 170);
+      stroke(132, 22, 77);
+      strokeWeight(7);
+      fill(255, 255, 255);
+      textSize(70);
+      textAlign(CENTER, CENTER);
+      text("YOU WIN", width / 2, height / 2);
+    }
+
+    // GAME OVER SCREEN
     //The following 7 lines of code was adpated from https://editor.p5js.org/skallywag/sketches/ByydCKx3m Accessed: 2024-02-17
     if (gameOverText) {
+      stroke(255);
+      fill(0);
+      rect(50, 200, 600, 170);
       stroke(132, 22, 77);
       strokeWeight(7);
       fill(255, 255, 255);
@@ -130,19 +149,28 @@ function draw() {
       text("GAME OVER", width / 2, height / 2);
     }
   }
+}
 
-  function gameOver() {
-    if (y >= 400 && speed >= 2.5) {
-      gameOverText = true;
-    }
+function gameOver() {
+  if (y >= 400 && speed >= 2.5) {
+    gameOverText = true;
+  } else if (y >= 400 && speed < 2.5) {
+    youWinText = true;
   }
 }
 
+// START SCREEN
 // the following 11 lines of code was adpated from https://chat.openai.com/share/b0da0a03-581c-4089-8b30-0478fad949d0 Accessed: 2024-02-19
-// start screen
+
 function keyPressed() {
   if (keyCode === ENTER && startScreen) {
     startScreen = false;
+  } else if (y === 400 && keyCode === ENTER) {
+    startScreen = true;
+    velocityY = 0;
+    speed = 0;
+    gameOverText = false;
+    youWinText = false;
   }
 }
 
@@ -152,6 +180,10 @@ function displayStartScreen() {
   strokeWeight(7);
   fill(255);
   textAlign(CENTER, CENTER);
-  textSize(40);
-  text("Press ENTER to start", width / 2, height / 2); // Display start message
+  textSize(60);
+  text("START GAME", width / 2, height / 2);
+  textSize(30);
+  text("press enter", width / 2, 350);
+
+  y = 50;
 }
